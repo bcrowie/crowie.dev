8713842c8ee150cda824a6f22a21618de0f2bbf2
Brenden Crowie <brendencrowie@gmail.com>
2020-06-16 14:31:29 -0400

    Added contact form functionality

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,13 +8,19 @@ const Contact = () => {
         <div className="border"></div>
         <h1>Contact</h1>
         <div className="contact-form">
-          <form action="">
-            <input type="name" placeholder="name"></input>
-            <input type="text" />
-            <input type="text" />
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+          <form action="https://formspree.io/mqkkzlje" method="POST">
+            <input type="name" placeholder="Name"></input>
+            <input type="text" placeholder="Email" />
+            <input type="tel" placeholder="Phone" />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button type="submit">Send</button>
           </form>
-          <button>Send</button>
         </div>
       </div>
     </div>
